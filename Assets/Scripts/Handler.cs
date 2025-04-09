65f92b0b5f3238062c7f6d69780def32a9161900
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2025-04-09 19:39:35 +0200

    d1

---
+++ Assets/Scripts/Handler.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Handler : MonoBehaviour
+{
+    public TileBoard board;
+    public CanvasGroup gameOver;
+    public CanvasGroup controlButtons;
+    public CanvasGroup menu;
+    public CanvasGroup scoreboard;
+    public TextMeshProUGUI highScoreText;
+    public TextMeshProUGUI scoreText;
+    public string difficulty;
+    public int score { get; private set; } = 0;
+
+    private void Start()
+    {
+        Menu();
+    }
+
+    public void Menu()
+    {
+        gameOver.alpha = 0;
+        gameOver.interactable = false;
+        controlButtons.alpha = 0;
+        controlButtons.interactable = false;
+        scoreboard.alpha = 0;
+        menu.alpha = 1;
+        menu.interactable = true;
+        difficulty = null;
+    }
+
+    public void StartEasy()
+    {
+        difficulty = "Easy";
+        NewGame();
+        
+    }
+    
+    public void StartMedium()
+    {
+        difficulty = "Medium";
+        NewGame();
+    }
+
+    public void StartHard()
+    {
+        difficulty = "Hard";
+        NewGame();
+    }
+    public void NewGame()
+    {
+        SetScore(0);
+        highScoreText.text = LoadHighScore(difficulty).ToString();
+        gameOver.alpha = 0;
+        gameOver.interactable = false;
+        menu.alpha = 0;
+        menu.interactable = false;
+        board.ClearGame();
+        board.SpawnStart();
+        scoreboard.alpha = 1;
+        controlButtons.alpha = 1;
+        controlButtons.interactable = true;
+        board.enabled = true;
+    }
+
+    public void GameOver()
+    {
+        board.enabled = false;
+        controlButtons.alpha = 0;
+        controlButtons.interactable = false;
+        gameOver.interactable = true;
+        StartCoroutine(FadeInOut(gameOver, 1f, 1f));
+    }
+
+    private IEnumerator FadeInOut(CanvasGroup canvasGroup, float to, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        
+        float time = 0f;
+        float dura = 0.5f;
+        float from = canvasGroup.alpha;
+
+        while (time < dura)
+        {
+            canvasGroup.alpha = Mathf.Lerp(from, to, time / dura);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        canvasGroup.alpha = to;
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+        SetHighScore();
+    }
+
+    private void SetHighScore()
+    {
+        int highscore = LoadHighScore(difficulty);
+        if (score > highscore)
+        {
+            PlayerPrefs.SetInt(difficulty, score);
+        }
+    }
+
+    private int LoadHighScore(string difficulty)
+    {
+        return PlayerPrefs.GetInt(difficulty, 0);
+    }
+
+    public void AddScore(int points)
+    {
+        SetScore(score + points);
+    }
+}
